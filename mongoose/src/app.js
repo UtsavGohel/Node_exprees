7daca2e3e7e53c2d516fa6bb7aa94fb88034b539
UtsavGohel <utsavgohel2002@gmail.com>
2021-08-21 13:50:25 +0530

    validation

--- mongoose/src/app.js
+++ mongoose/src/app.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
     // create connection with mongodv=b in node.js
 mongoose.connect("mongodb://localhost:27017/byVsCode", { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("succesfull"))
@@ -10,11 +11,24 @@ const playlistSchema = new mongoose.Schema({
         name: {
             type: String,
             //here if we want to need value than we add require field
-            required: true
+            required: true,
+            lowercase: true,
+            trim: true,
+            minlength: [2, "minimum 2 letter to enter"],
+            maxlength: [30, "minimum 30 letter to enter"]
         },
         ctype: String,
         author: String,
         active: Boolean,
+        email: {
+            type: String,
+            require: true,
+            validate(value) {
+                if (!validator.isEmail(value)) {
+                    throw new Error('Email Invalid')
+                }
+            }
+        },
         date: {
             type: Date,
             default: Date.now
@@ -26,33 +40,35 @@ const Playlist = new mongoose.model('playlist', playlistSchema);
 
 // create document or insert
 const craeteDocument = async() => {
-        try {
-            const reactPlaylist = new Playlist({
-                name: "React JS",
-                ctype: "Front-end",
-                author: "Hemangini",
-                active: true
-            })
-            const NodePlaylist = new Playlist({
-                name: "Node JS",
-                ctype: "Back-end",
-                author: "Utsav",
-                active: true
-            })
-            const mongoosePlaylist = new Playlist({
-                name: "mongoose",
-                ctype: "Database",
-                author: "Hemangini",
-                active: true
-            })
-            const result = await Playlist.insertMany([mongoosePlaylist, NodePlaylist, reactPlaylist])
-            console.log(result)
+    try {
+        // const reactPlaylist = new Playlist({
+        //     name: "React JS",
+        //     ctype: "Front-end",
+        //     author: "Hemangini",
+        //     active: true
+        // })
+        // const NodePlaylist = new Playlist({
+        //     name: "Node JS",
+        //     ctype: "Back-end",
+        //     author: "Utsav",
+        //     active: true
+        // })
+        const mongoosePlaylist = new Playlist({
+            name: "mongoose",
+            ctype: "Database",
+            author: "Hemangini",
+            email: "abc@gmail.com",
+            active: true
 
-        } catch (err) {
-            console.log(err)
-        }
+        })
+        const result = await Playlist.insertMany([mongoosePlaylist])
+        console.log(result)
+
+    } catch (err) {
+        console.log(err)
     }
-    // craeteDocument()
+}
+craeteDocument()
 const getDocument = async() => {
     try {
         const result = await Playlist
